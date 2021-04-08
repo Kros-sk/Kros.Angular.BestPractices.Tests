e918dc53a8564649436d423d4e39c017c703e382
mikulamarian <marian.mikula@kros.sk>
2021-04-08 13:07:33 +0200

    update first feature

--- cypress/integration/step-definitions/first.ts
+++ cypress/integration/step-definitions/first.ts
@@ -1,8 +1,18 @@
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 
 
 Given(`som všetko dobre nastavil`, () => {});
 
 Then(`všetko funguje a otvorí mi Google!`, () => {
   cy.visit('https://www.google.com')
+});
+
+When('začnem vyhľadávať', () => {
+  cy.get('#zV9nZe').click();
+  cy.get('.gLFyf').type("Skúsim štastie{enter}");
+})
+
+Then('sa mi zobrazia výsledky', () => {
+  cy.get('#rcnt');
+  cy.location('pathname').should('contain', '/search');
 });
